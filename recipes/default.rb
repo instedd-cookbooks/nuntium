5696f880c63276760f7affccf313213efe36635f
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-08-21 19:07:56 -0300

    Configure rails app and install services with foreman

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,6 @@
 package "git"
 
+# Install rbenv and ruby
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
@@ -8,25 +9,54 @@ rbenv_gem "bundler" do
   ruby_version "1.9.3-p484"
 end
 
+# Install MySQL
 include_recipe "mysql::server"
 include_recipe "mysql::client"
 include_recipe "database::mysql"
 
+mysql_connection = {
+  host: 'localhost',
+  username: 'root',
+  password: node['mysql']['server_root_password']
+}
+
 mysql_database "nuntium" do
-  connection(
-    host: 'localhost',
-    username: 'root',
-    password: node['mysql']['server_root_password']
-  )
+  connection mysql_connection
 end
+
+include_recipe "nodejs"
 
 application "nuntium" do
   path "/u/apps/nuntium"
   repository "https://bitbucket.org/instedd/nuntium.git"
 
+  rails do
+    bundler true
+    bundle_command "#{node[:rbenv][:root_path]}/shims/bundle"
+
+    database(
+      database: "nuntium",
+      adapter: "mysql2",
+      username: "root",
+      password: node['mysql']['server_root_password']
+    )
+  end
+
   before_symlink do
-    execute "install bundle" do
-      command "/opt/rbenv/shims/bundle install --path #{shared_path}/bundle --jobs=8 --deployment --without development test"
+    rbenv_execute "create database" do
+      command "bundle exec rake db:migrate"
+      environment "RAILS_ENV" => "production"
+      cwd release_path
+    end
+  end
+
+  before_restart do
+    file "#{release_path}/.env" do
+      content "RAILS_ENV=production"
+    end
+
+    rbenv_execute "foreman export" do
+      command "bundle exec foreman export -a nuntium -u root -c 'worker_fast=1,worker_slow=1,xmpp=1,smpp=1,msn=1,cron=1,sched=1' upstart /etc/init"
       cwd release_path
     end
   end
